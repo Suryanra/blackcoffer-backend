84357ade964d641d6c6052db7daab045ac85ae40
suryanra <suryapratapnra@gmail.com>
2024-05-30 19:31:20 +0530

    msg of commitmend

---
+++ App.js
@@ -0,0 +1,27 @@
+const express=require('express')
+const app=express();
+const {dbConnect}=require('./config/database')
+dbConnect();
+const BlackCofferModel =require('./models/BlackCofferDataset')
+var cors = require('cors')
+
+
+app.use(cors({
+  origin: 'http://localhost:3000', // No trailing slash
+  credentials: true
+}));
+
+app.get('/',async(req,resp)=>{
+      const data=await BlackCofferModel.find({});
+      console.log(data);
+resp.send(data);
+})
+
+
+
+
+app.listen(3001,()=>
+{
+console.log("server listening at 3001 port");
+})
+
